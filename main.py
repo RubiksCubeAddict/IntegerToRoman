d0f480348cbdee6184cdea5fdb9fea3c64a59376
RubiksCubeAddict <yardenakerman2011@gmail.com>
2025-08-16 23:20:37 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+def int_to_roman(num):
+    """ Converts an integer to a roman numeral.
+       Supports any positive integer """
+    
+    if not isinstance(num, int):
+        raise TypeError("Input must be an integer.")
+    if num <= 0:
+        raise ValueError("Input must be a positive integer.")
+
+    
+    val = [
+        1000,900,500,400,
+        100,90,50,40,
+        10,9,5,4,1
+    ]
+    syms = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV", "I"
+    ]
+
+    roman_num = ""
+    i = 0
+    while num > 0:
+        for _ in range(num // val[i]):
+            roman_num += syms[i]
+            num -= val[i]
+        i += 1
+    return roman_num
